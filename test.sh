8d1e697a0a50851eb066f0aec7409d9b85ebe2d7
Hibiki Okada <4617016@ed.tus.ac.jp>
2020-06-06 17:17:26 +0900

    fixed test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-pytest -v --cov=42cli --cov-report=html 
+poetry run pytest -v --cov=42cli --cov-report=html 
 if [ "$(uname)" == "Darwin" ]; then
     open ./htmlcov/index.html
 elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
