14835667c65752e56504ea5718fd8b733ee9f47a
Hibiki Okada <4617016@ed.tus.ac.jp>
2020-06-10 02:05:02 +0900

    fixed 42 status

--- 42cli/status.py
+++ 42cli/status.py
@@ -2,19 +2,44 @@
 # -*- coding: utf=8 -*-
 
 import importlib
+from datetime import datetime
 
 authorize = importlib.import_module("42cli.authorize")
 api = importlib.import_module("42cli.api")
+const = importlib.import_module("42cli.const")
 
 
 def showStatus():
     authInfo = authorize.getAuthInfo()
     apiResponse = api.apiGetUsers(authInfo)
 
-    print('login: ' + apiResponse['login'])
-    print('email: ' + apiResponse['email'])
+    # basic information
+    print('{}<{}>'.format(apiResponse['login'], apiResponse['email']))
+    # evaluation point
     print('Eval points: ' + str(apiResponse['correction_point']))
+
+    # black hole date
+    for cursus in apiResponse['cursus_users']:
+        if cursus['blackholed_at'] is not None:
+            blackHole = datetime.strptime(
+                cursus['blackholed_at'],
+                '%Y-%m-%dT%H:%M:%S.%fZ'
+            )
+            limit = blackHole - datetime.now()
+            print("{}: {} days left".format(cursus['cursus']['name'],
+                                            limit.days))
+
+    # project in progress
+    projects = []
+    for project in apiResponse['projects_users']:
+        if project['status'] == 'in_progress':
+            projects.append(project['project']['name'])
+    if len(projects) == 0:
+        print(const.MSG_NO_PROJECT_IN_PROGRESS)
+    for project in projects:
+        print(project)
+
+    # optionで管理 -aで全て
     # cursus毎にレベル、スキルなど表示したい!
     # termgraph
-    for cursus in apiResponse['cursus_users']:
-        print(cursus['cursus']['name'], ': ', cursus['blackholed_at'])
+    # review予定もここで
